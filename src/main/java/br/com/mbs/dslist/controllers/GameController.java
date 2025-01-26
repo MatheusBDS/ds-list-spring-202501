f3ef325b20ea8a367f5d88a05200a1f05e786806
Matheus Silva <37903440+MatheusBDS@users.noreply.github.com>
2025-01-26 14:30:27 -0300

    Implementing get game by id

--- src/main/java/br/com/mbs/dslist/controllers/GameController.java
+++ src/main/java/br/com/mbs/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package br.com.mbs.dslist.controllers;
 
+import br.com.mbs.dslist.dto.GameDto;
 import br.com.mbs.dslist.dto.GameMinDto;
 import br.com.mbs.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class GameController {
     @GetMapping
     public List<GameMinDto> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id) {
+        return gameService.findById(id);
     }
 }
--- src/main/java/br/com/mbs/dslist/services/GameService.java
+++ src/main/java/br/com/mbs/dslist/services/GameService.java
@@ -1,9 +1,11 @@
 package br.com.mbs.dslist.services;
 
+import br.com.mbs.dslist.dto.GameDto;
 import br.com.mbs.dslist.dto.GameMinDto;
 import br.com.mbs.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,9 +15,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll() {
         return gameRepository.findAll().stream()
                 .map(GameMinDto::new)
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id){
+        return new GameDto(gameRepository.findById(id).get());
     }
 }
